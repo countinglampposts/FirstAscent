e6b3561544abf4c4355a3326ec49a1b08bf6dedb
Chris <countinglampposts@gmail.com>
2019-05-02 00:00:23 -0600

    Added some seeded randomness to the perlin noise

--- TerraGen/Assets/Scripts/Data/PerlinLayerData.cs
+++ TerraGen/Assets/Scripts/Data/PerlinLayerData.cs
@@ -11,15 +11,18 @@ namespace TerraGen.Data
         public float frequency = .0005f;
         public float amplitude = 1000f;
         public int octaves = 5;
+        public int seed;
 
         public float ApplyLayer(float x, float y, float height)
         {
+            var rnd = new System.Random(seed);
             for (float o = 1; o <= octaves; o++)
             {
                 var usedFrequency = frequency * o;
                 var usedAmplitude = amplitude / o;
+                var offset = new Vector2(rnd.Next(1000), rnd.Next(1000)) / 1000f;
 
-                height += Mathf.PerlinNoise(x * usedFrequency, y * usedFrequency) * usedAmplitude;
+                height += Mathf.PerlinNoise(x * usedFrequency + offset.x, y * usedFrequency + offset.y) * usedAmplitude;
             }
 
             return height;
