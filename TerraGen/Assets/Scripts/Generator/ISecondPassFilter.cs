cd830f3c2ef92c8109ae7936c4c497d263af1431
Chris <countinglampposts@gmail.com>
2019-05-06 22:49:55 -0600

    Added normalization layer

---
+++ TerraGen/Assets/Scripts/Generator/ISecondPassFilter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerraGen.Generator
+{
+    public interface ISecondPassFilter
+    {
+        float[,] ApplyLayer(float[,] terrainData);
+    }
+}
---
+++ TerraGen/Assets/Scripts/Generator/Layers/NormalizeLayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TerraGen.Generator;
+using UnityEngine;
+
+namespace TerraGen.Generator
+{
+    public class NormalizeLayer : ISecondPassFilter
+    {
+        [SerializeField] float maxY;
+
+        public float[,] ApplyLayer(float[,] terrainData)
+        {
+            var min = float.MaxValue;
+            var max = float.MinValue;
+            foreach (var point in terrainData)
+            {
+                min = Mathf.Min(min, point);
+                max = Mathf.Max(max, point);
+            }
+
+            for (int x = 0; x < terrainData.GetLength(0); x++)
+            {
+                for (int y = 0; y < terrainData.GetLength(1); y++)
+                {
+                    terrainData[x, y] = Mathf.InverseLerp(min, max, terrainData[x, y]) * maxY;
+                }
+            }
+
+            return terrainData;
+        }
+    }
+}
